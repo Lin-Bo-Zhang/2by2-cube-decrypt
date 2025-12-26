68d36b21952ce006ce289ecb13b94c96326de2a8
Lin-Bo-Zhang <JAY0411JCEM@GMAIL.COM>
2025-12-26 23:01:57 +0800

    Update moves.py

--- moves.py
+++ moves.py
@@ -1,6 +1,9 @@
+'''
+程式.二階魔術方塊破解器.moves 的 Docstring
+本程式用於存放重複類似的魔術方塊轉動函式，以便 main.py 調用。
+'''
 # 轉動代號和函式對照 
 # R=R0 R'=R1 L=L0 L'=L1 U=U0 U'=U1 D=D0 D'=D1 F=F0 F'=F1 B=B0 B'=B1
-#temporary storage=t
 import numpy as np
 l = np.array([0,1,2,3,4,5,6,7])
 o = np.array([0,0,0,0,0,0,0,0])
@@ -62,7 +65,7 @@ def U0(l,o): #U
             o[[idx]]=o[[idx]]-1
     return l,o
 
-def U1(l,o):
+def U1(l,o): #U'
     a=[5,6,7,4]
     l[[4,5,6,7]]=l[a]
     o[[4,5,6,7]]=o[a]
@@ -73,7 +76,7 @@ def U1(l,o):
             o[[idx]]=o[[idx]]-1
     return l,o
 
-def D0(l,o):
+def D0(l,o):#D
     a=[1,2,3,0]
     l[[0,1,2,3]]=l[a]
     o[[0,1,2,3]]=o[a]
@@ -84,7 +87,7 @@ def D0(l,o):
             o[[idx]]=o[[idx]]-1
     return l,o
 
-def D1(l,o):
+def D1(l,o):#D'
     a=[3,0,1,2]
     l[[0,1,2,3]]=l[a]
     o[[0,1,2,3]]=o[a]
@@ -95,7 +98,7 @@ def D1(l,o):
             o[[idx]]=o[[idx]]-1
     return l,o
 
-def F0(l,o):
+def F0(l,o):#F
     a=[4,0,1,5]
     l[[0,1,5,4]]=l[a]
     o[[0,1,5,4]]=o[a]
@@ -106,7 +109,7 @@ def F0(l,o):
             o[[idx]]=o[[idx]]-2
     return l,o
 
-def F1(l,o):
+def F1(l,o):#F'
     a=[1,5,4,0]
     l[[0,1,5,4]]=l[a]
     o[[0,1,5,4]]=o[a]
@@ -117,7 +120,7 @@ def F1(l,o):
             o[[idx]]=o[[idx]]-2
     return l,o
 
-def B0(l,o):
+def B0(l,o):#B
     a=[6,2,3,7]
     l[[2,3,7,6]]=l[a]
     o[[2,3,7,6]]=o[a]
@@ -128,7 +131,7 @@ def B0(l,o):
             o[[idx]]=o[[idx]]-2
     return l,o
 
-def B1(l,o):
+def B1(l,o):#B'
     a=[3,7,6,2]
     l[[2,3,7,6]]=l[a]
     o[[2,3,7,6]]=o[a]
@@ -139,36 +142,36 @@ def B1(l,o):
             o[[idx]]=o[[idx]]-2
     return l,o
 
-def N(l,o):
+def N(l,o):#不動作(佔位用)
     return l,o
 
-def R2(l,o):
+def R2(l,o):#R2
     R0(l,o)
     R0(l,o)
     return l,o
-def U2(l,o):
+def U2(l,o):#U2
     U0(l,o)
     U0(l,o)
     return l,o
-def F2(l,o):
+def F2(l,o):#F2
     F0(l,o)
     F0(l,o)
     return l,o
-def L2(l,o):
+def L2(l,o):#L2
     L0(l,o)
     L0(l,o)
     return l,o
-def D2(l,o):
+def D2(l,o):#D2
     D0(l,o)
     D0(l,o)
     return l,o
-def B2(l,o):
+def B2(l,o):#B2
     B0(l,o)
     B0(l,o)
     return l,o
-def f(l,o,a):
+def f(l,o,a):#執行列表動作
     for i in a:
         i(l,o)
     return l,o
 if __name__=="__main__":
-    print()
+    print('What are you doing?')
